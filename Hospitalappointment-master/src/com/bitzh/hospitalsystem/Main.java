9a1013eebbd463c4dfce806ca403e43b17fb689f
Simon <2507486301@qq.com>
2024-06-06 15:08:05 +0800

    1

--- Hospitalappointment-master/src/com/bitzh/hospitalsystem/Main.java
+++ Hospitalappointment-master/src/com/bitzh/hospitalsystem/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             LoginFrame loginFrame = new LoginFrame();
-            loginFrame.setVisible(true);
+            loginFrame.setVisible(true)
         });
     }
 }
